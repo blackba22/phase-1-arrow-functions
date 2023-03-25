f87d04d72a9866c056c8912867098a90e39b352d
+ <+>
2023-03-24 20:32:17 -0400

    arrow functions upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+divide(2000,100);
+
+const square = x => x * 2
+square(4);
+
+const add =(num3, num4) => {
+    return num3 + num4;
+}
+add(3,4);
